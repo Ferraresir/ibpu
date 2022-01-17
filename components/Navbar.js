f1807721b893d542e87679cb88ad10a7cb7b0296
Ramiro Ferraresi <ramiro_ferraresi@hotmail.com>
2022-01-17 06:17:29 -0300

    favicon fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <header className="relative w-full px-8 text-gray-700 bg-white body-font">
       <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-        <a
-          href="#_"
+        <Link href='/'>
+        <a 
           className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none"
         >
         <Image className="ml-2" src='/logo.png' width={40} height={40}/>
         Ibpu
         </a>
-
+        </Link>
         <nav className="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
           <a
             href="#_"
